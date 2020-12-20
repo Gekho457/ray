daae7a92de85b9f77a0bc173041931f9d408c0d6
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2020-12-19 16:49:39 -0800

    Artifact test

--- .buildkite/build-linux-wheels.sh
+++ .buildkite/build-linux-wheels.sh
@@ -54,7 +54,6 @@ nvm install "$NODE_VERSION"
 nvm use "$NODE_VERSION"
 
 # Build the dashboard so its static assets can be included in the wheel.
-# TODO(mfitton): switch this back when deleting old dashboard code.
 pushd dashboard/client
   npm ci
   npm run build
@@ -76,13 +75,13 @@ for ((i=0; i<${#PYTHONS[@]}; ++i)); do
     # Fix the numpy version because this will be the oldest numpy version we can
     # support.
     /opt/python/"${PYTHON}"/bin/pip install -q numpy=="${NUMPY_VERSION}" cython==0.29.15
-    # Set the commit SHA in __init__.py.
-    # if [ -n "$TRAVIS_COMMIT" ]; then
-      # sed -i.bak "s/{{RAY_COMMIT_SHA}}/$TRAVIS_COMMIT/g" ray/__init__.py && rm ray/__init__.py.bak
-    # else
-      # echo "TRAVIS_COMMIT variable not set - required to populated ray.__commit__."
-      # exit 1
-    # fi
+    Set the commit SHA in __init__.py.
+    if [ -n "$BUILDKITE_COMMIT" ]; then
+      sed -i.bak "s/{{RAY_COMMIT_SHA}}/$BUILDKITE_COMMIT/g" ray/__init__.py && rm ray/__init__.py.bak
+    else
+      echo "BUILDKITE_COMMIT variable not set - required to populated ray.__commit__."
+      exit 1
+    fi
 
     PATH=/opt/python/${PYTHON}/bin:/root/bazel-3.2.0/output:$PATH \
     /opt/python/"${PYTHON}"/bin/python setup.py bdist_wheel
